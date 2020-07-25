b62d2b4b115b9bfe0ceef0ccaefa5b18f577a6ba
Yuki Nagae <yuki.nagae1130@gmail.com>
2020-07-25 17:56:22 +0900

    sample module web server

---
+++ example/http_server/main.tf
@@ -0,0 +1,41 @@
+variable "instance_type" {
+}
+
+output "public_dns" {
+  value = aws_instance.default.public_dns
+}
+
+//
+// resources
+//
+resource "aws_instance" "default" {
+  ami                    = "ami-06ad9296e6cf1e3cf"
+  vpc_security_group_ids = [aws_security_group.default.id]
+  instance_type          = var.instance_type
+
+  user_data = <<EOF
+#!/bin/bash
+yum install -y httpd
+systemctl start httpd.service
+  EOF
+}
+
+resource "aws_security_group" "default" {
+  name = "ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
