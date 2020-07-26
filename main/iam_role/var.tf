5b6ffa81d4e3d47f1454f8719d998924dcb56017
Yuki Nagae <yuki.nagae1130@gmail.com>
2020-07-26 15:41:35 +0900

    Add IAM module and sample

---
+++ main/iam_role/var.tf
@@ -0,0 +1,11 @@
+variable "name" {}
+variable "policy" {}
+variable "identifier" {}
+
+output "iam_role_arn" {
+  value = aws_iam_role.default.arn
+}
+
+output "iam_role_name" {
+  value = aws_iam_role.default.name
+}
